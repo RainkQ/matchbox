442542e7cc8d2d99720a09dabba1210df6d12518
Tnicy <ashyoumo@gmail.com>
2019-04-05 16:52:53 +0800

    从头来。。

--- src/main/java/tk/tnicy/matchbox/domain/SysPermissionRepository.java
+++
@@ -1,8 +0,0 @@
-package tk.tnicy.matchbox.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tk.tnicy.matchbox.domain.auth.SysPermission;
-
-public interface SysPermissionRepository extends JpaRepository<SysPermission, Long> {
-
-}
--- src/main/java/tk/tnicy/matchbox/domain/UserInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package tk.tnicy.matchbox.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tk.tnicy.matchbox.domain.auth.UserInfo;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-}
