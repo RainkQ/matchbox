f3dd5f9b4ae6d4591827331377fbadf4311eb6f3
Tnicy <ashyoumo@gmail.com>
2019-04-06 20:56:46 +0800

    基本css确定

--- src/main/java/tk/tnicy/matchbox/web/HomeController.java
+++ src/main/java/tk/tnicy/matchbox/web/HomeController.java
@@ -2,8 +2,12 @@ package tk.tnicy.matchbox.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import tk.tnicy.matchbox.domain.User;
 import tk.tnicy.matchbox.domain.UserRepository;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -12,7 +16,11 @@ public class HomeController {
     UserRepository userRepository;
 
     @GetMapping("/")
-    public String getIndex() {
+    public String getIndex(HttpSession session, Model model) {
+        User user = new User();
+        user.setUsername("未登录");
+        model.addAttribute("user", user);
+
         return "index";
     }
 }
