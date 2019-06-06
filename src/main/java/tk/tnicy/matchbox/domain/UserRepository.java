0d6ebc6e3e3ee4cb8c62ff26f27f4a292d4cdaa7
lxw <806888122@qq.com>
2019-06-07 00:07:57 +0800

    22

--- src/main/java/tk/tnicy/matchbox/domain/UserRepository.java
+++ src/main/java/tk/tnicy/matchbox/domain/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -30,4 +32,20 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserById(Long id);
 
+
+    @Query(value = "WITH seq_a AS ( SELECT id_a, similarity FROM similarityinfo WHERE id_b = :id ),\n" +
+            "seq_b AS ( SELECT id_b, similarity FROM similarityinfo WHERE id_a = :id ),\n" +
+            "seq ( ID, similarity ) AS ( SELECT * FROM ( SELECT * FROM seq_a UNION SELECT * FROM seq_b ) AS tem ),\n" +
+            "userwithsimilarity AS ( SELECT * FROM ( seq NATURAL JOIN feature ) ) SELECT ID\n" +
+            ",\n" +
+            "gender,\n" +
+            "signature,\n" +
+            "avataruuid,\n" +
+            "avatar_id \n" +
+            "FROM\n" +
+            "\tuserwithsimilarity \n" +
+            "ORDER BY\n" +
+            "\tsimilarity DESC;",
+            nativeQuery = true)
+    List<Feature> findUserBySimilarity(@Param("id") Long id);
 }
